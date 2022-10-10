b1fac961ff2aab558f5b57a6c0edfa7cda867721
Irina946 <Irina.Larina@urfu.me>
2022-10-10 10:32:58 +0500

    module5-task1: Создана модульность

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-function selectNumberRange(min, max) {
-  if (min < 0 || max <= min) {
-    throw 'You passed invalid arguments';
-  }
+import {generatePhotosData} from './photos';
 
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function checkStringLength(comment, maxLength) {
-  return comment.length <= maxLength;
-}
-
-
-selectNumberRange(5, 10);
-checkStringLength('Строка', 8);
+console.log(generatePhotosData());
